13d4d926a8138aab37fa57b8b192bf7d1349f7d6
sabina <bahodirova2000@gmail.com>
2023-01-24 14:45:20 -0500

    sales forms finished

---
+++ ghi/app/src/CustomerForm.js
@@ -0,0 +1,79 @@
+import React, { useState} from 'react'
+
+
+function CustomerForm () {
+    const [customerName, setCustomerName] = useState("")
+    const handleCustomerNameChange = (e) => {
+        setCustomerName(e.target.value)
+    }
+
+    const [address, setAddress] = useState("")
+    const handleAddressChange = (e) => {
+        setAddress(e.target.value)
+    }
+
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const handlePhoneNumberChange = (e) => {
+        setPhoneNumber(e.target.value)
+    }
+
+
+    const handleSubmit = async (event) => {
+      event.preventDefault()
+
+      const data = {}
+      data.name = customerName
+      data.address = address
+      data.phone_number = phoneNumber
+
+      const url = `http://localhost:8090/api/customers/`
+
+      const fetchConfig = {
+        method: "post",
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+
+      const response = await fetch(url, fetchConfig)
+
+      if (response.ok) {
+        const newCustomer = await response.json()
+        console.log(newCustomer)
+
+        setCustomerName("")
+        setAddress("")
+        setPhoneNumber("")
+      }
+    }
+
+
+
+    return (
+      <div className="row">
+        <div className="offset-3 col-6">
+          <div className="shadow p-4 mt-4">
+            <h1>Add a new customer</h1>
+            <form onSubmit={handleSubmit} id="create-customer-form">
+              <div className="form-floating mb-3">
+                <input onChange={handleCustomerNameChange} value={customerName} placeholder="Name" required name="name" className="form-control" />
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input onChange={handleAddressChange} value={address} placeholder="Address" required name="address" type="text" id="address" className="form-control" />
+                <label htmlFor="address">Address</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input onChange={handlePhoneNumberChange} value={phoneNumber} placeholder="Phone Number" required name="phone_number" type="number" id="phone_number" className="form-control" />
+                <label htmlFor="phone_number">Phone Number</label>
+              </div>
+              <button className="btn btn-primary">Add</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default CustomerForm
---
+++ ghi/app/src/SalesList.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react"
+
+function SalesList() {
+
+  const [sales, setSales] = useState([])
+  const getData = async ()=> {
+    const response = await fetch('http://localhost:8090/api/sales/')
+    if (response.ok) {
+      const data = await response.json()
+      setSales(data.sales)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+
+    return (
+        <table className = "table table-hover">
+        <thead>
+          <tr>
+            <th>Sales Associate</th>
+            <th>Employee Number</th>
+            <th>Purchaser Name</th>
+            <th>VIN</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+        {sales.map(sale => {
+          return (
+            <tr key={ sale.id }>
+              <td>{ sale.sales_person.name }</td>
+              <td>{ sale.sales_person.employee_number }</td>
+              <td>{ sale.customer }</td>
+              <td>{ sale.automobile.vin }</td>
+              <td>{ sale.sale_price }</td>
+            </tr>
+          );
+        })}
+
+        </tbody>
+      </table>
+    )
+}
+export default SalesList
