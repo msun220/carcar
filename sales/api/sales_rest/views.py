f8a617e311462c4fdf74688e252f8bb2df46c907
Matthew <mattsun220@gmail.com>
2023-01-25 16:03:00 -0500

    imports fixed

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 import json
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from .models import Appointment, Technician
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
