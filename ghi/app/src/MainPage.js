6146d24e37ada3be0cfaa1bc6a7e13b85e3f94f0
sabina <bahodirova2000@gmail.com>
2023-01-31 18:14:44 -0500

    added buttons to main.js

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,13 +1,28 @@
 function MainPage() {
   return (
-    <div className="card text-bg text-white">
-      <img src="https://wallpaperaccess.com/full/2503410.jpg" className="card-img img-fluid" />
-      <div className="card-img-overlay">
-        <h5 className="card-title">CarCar</h5>
-        <p className="card-text">The premiere solution for automobile dealership
-            management!</p>
+    <div className="carousel-inner">
+      <img src='https://wallpaperaccess.com/full/2503410.jpg' className='opacity-50 w-100' />
+      <div className="carousel-caption d-none d-md-block justify-content-center h-75">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <button type="button" style={{ width: "300px", height: "150px",}} className="btn btn-outline-dark">Can you see me?</button>
+            </div>
+            <div className="col">
+              <button type="button" style={{ width: "300px", height: "150px",}} className="btn btn-outline-dark">Can you see me?</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col" style={{paddingTop: 15, paddingBottom: 12}}>
+              <button type="button" style={{ width: "300px", height: "150px",}} className="btn btn-outline-dark">Can you see me?</button>
+            </div>
+            <div className="col" style={{paddingTop: 15, paddingBottom: 12}}>
+              <button type="button" style={{ width: "300px", height: "150px",}} className="btn btn-outline-dark">Can you see me?</button>
+            </div>
+          </div>
       </div>
-    </div>
+      </div>
+      </div>
   );
 }
 
